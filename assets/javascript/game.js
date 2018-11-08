bd03b3eace936190f86f72e5a953bc809f390764
Jackson Sabol <JacksonSabol@users.noreply.github.com>
2018-11-08 09:03:44 -0800

    add initial variables for words to choose from, wins, losses, turns left, and word index

---
+++ assets/javascript/game.js
@@ -0,0 +1,14 @@
+// Array of words to choose from
+var wordsArray = ["spaceship", "planet", "orbit", "railgun", "alien", "invasion", "warpspeed", "reactor", "shields", "bridge"];
+
+// Start the game with a score of 0
+var score = 0;
+
+// Variable to hold the index of current word
+var wordIndex = 0;
+
+// Setting variables for wins, losses, and number of guesses remaining
+var wins = 0;
+var losses = 0;
+var guessesRemaining = 10;
+
